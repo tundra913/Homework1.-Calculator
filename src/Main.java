2f2ae703866d572d4cac7e323299a043ef8078cc
Ivanova Irina <ivanovairishav@ya.ru>
2021-09-15 11:07:01 +0300

    correction 1

--- src/Main.java
+++ src/Main.java
@@ -5,8 +5,7 @@ public class Main {
         int a = calc.plus.apply(1, 2);
         int b = calc.minus.apply(1, 1);
         int c = calc.devide.apply(a, b); //выбрасывается исключение, деление на ноль.
-        // можно отработать через try catch, а можно и через тернарный оператор
-        // BinaryOperator<Integer>devide = (x, y) -> y == 0 ? y : x / y;
+        // отработала через try/catch в лямбда-выражении.
 
         calc.println.accept(c);
     }
