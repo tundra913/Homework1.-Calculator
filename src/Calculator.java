325aef7086cec3f2f31ad6f6fd4e7f59d1fdb5f5
Ivanova Irina <ivanovairishav@ya.ru>
2021-09-15 11:11:47 +0300

    correction 1.1(revision)

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,18 +1,17 @@
 import java.util.function.*;
 
-public class Calculator extends RuntimeException {
+public class Calculator {
     static Supplier<Calculator> instance = Calculator::new;
 
     BinaryOperator<Integer> plus = (x, y) -> x + y;
     BinaryOperator<Integer> minus = (x, y) -> x - y;
     BinaryOperator<Integer> multiply = (x, y) -> x * y;
     BinaryOperator<Integer> devide = (x, y) -> {
-        if(y == 0) {
+        if (y == 0) {
             try {
                 throw new ArithmeticException();
-            }
-            catch (ArithmeticException e) {
-                System.out.println("Вы делите на ноль!");
+            } catch (ArithmeticException e) {
+                System.out.println("Argument 'divisor' is 0");
             }
         }
         return x / y;
